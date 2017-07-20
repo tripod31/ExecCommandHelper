ddb0738b0daa6d31f0da7722bb5f83d2cd6844b9
tripod31 <tripod31@hotmail.com>
2017-07-20 15:20:14 +0900

    change folder

---
+++ ExecCommand.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ExecCommandHelper
+{
+    /*
+        非同期にコマンドを実行  
+    */
+    public class ExecCommand
+    {
+
+        private Process _process;
+        private int _errcode = 0;
+        private string _errmsg = "";
+        private string _stdout = "";
+        private string _stderr = "";
+        private Encoding _encoding=Encoding.UTF8;
+
+        public Process process
+        {
+            get { return _process; }
+        }
+        public int errcode
+        {
+            get { return _errcode; }
+        }
+        public string errmsg
+        {
+            get { return _errmsg; }
+        }
+        public string stdout
+        {
+            get { return _stdout; }
+        }
+        public string stderr
+        {
+            get { return _stderr; }
+        }
+        public Encoding Encoding
+        {
+            get { return _encoding; }
+            set
+            {
+                _encoding = value;
+            }
+        }
+
+        public ExecCommand()
+        {
+        }
+
+        // コマンドを非同期を実行
+        public int ExecAsync(string exe_file, string args,string exec_dir,Form form=null,EventHandler exited_handler=null)
+        {
+            _errcode = 0;
+            _errmsg = "";
+            _stdout = "";
+            _stderr = "";
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.RedirectStandardInput = false;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                psi.FileName = exe_file;
+                psi.Arguments = args;
+                psi.WorkingDirectory = exec_dir;
+                psi.StandardOutputEncoding = _encoding;
+                psi.StandardErrorEncoding = _encoding;
+
+                _process = new System.Diagnostics.Process();
+                _process.StartInfo = psi;
+                //OutputDataReceivedイベントハンドラを追加
+                _process.OutputDataReceived += p_OutputDataReceived;
+                _process.ErrorDataReceived += p_ErrorDataReceived;
+
+                if (form!=null && exited_handler != null)
+                {
+                    //イベントハンドラがフォームを作成したスレッドで実行されるようにする
+                    _process.SynchronizingObject = form;
+                    //イベントハンドラの追加
+                    _process.Exited += exited_handler;
+                    //プロセスが終了したときに Exited イベントを発生させる
+                    _process.EnableRaisingEvents = true;
+                }
+
+                _stdout = "";
+                _stderr = "";
+
+                _process.Start();
+
+                //非同期で出力の読み取りを開始
+                _process.BeginOutputReadLine();
+                _process.BeginErrorReadLine();
+
+                //p.WaitForExit();
+
+            }
+            catch (Exception e)
+            {
+                _errmsg = e.Message;
+                _errcode = -1;
+            }
+            return _errcode;
+        }
+
+        // コマンドを同期実行
+        public int ExecSync(string exe_file, string args, string exec_dir)
+        {
+            _errcode = 0;
+            _errmsg = "";
+            _stdout = "";
+            _stderr = "";
+
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.RedirectStandardInput = false;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardError = true;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                psi.FileName = exe_file;
+                psi.Arguments = args;
+                psi.WorkingDirectory = exec_dir;
+
+                _process = new System.Diagnostics.Process();
+                _process.StartInfo = psi;
+
+                _stdout = "";
+                _stderr = "";
+
+                _process.Start();
+
+                //非同期で出力の読み取りを開始
+                _stdout=_process.StandardOutput.ReadToEnd();
+                _stderr = _process.StandardError.ReadToEnd();
+
+                _process.WaitForExit();
+
+            }
+            catch (Exception e)
+            {
+                _errmsg = e.Message;
+                _errcode = -1;
+            }
+            return _errcode;
+        }
+
+        //OutputDataReceivedイベントハンドラ
+        //行が出力されるたびに呼び出される
+        private void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            if (_stdout.Length > 0)
+                _stdout += Environment.NewLine;
+            _stdout += e.Data;
+        }
+        //ErrorDataReceivedイベントハンドラ
+        private void p_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            if (_stderr.Length > 0)
+                _stderr += Environment.NewLine;
+            _stderr += e.Data;
+        }
+        private void p_Exited(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExecCommandInfo.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ExecCommandHelper
+{
+    public class ExecCommandInfoCtrl
+    {
+        private List<ExecCommandInfo> _infos;
+        public List<ExecCommandInfo> Infos
+        {
+            get { return _infos; }
+        }
+
+        public ExecCommandInfoCtrl()
+        {
+            _infos = new List<ExecCommandInfo>();
+        }
+
+        public ExecCommandInfo get_info(string name)
+        {
+            return _infos.FirstOrDefault((ExecCommandInfo elem) => elem.name == name);
+        }
+
+        public void save_info(ExecCommandInfo info)
+        {
+            ExecCommandInfo obj = this._infos.FirstOrDefault((ExecCommandInfo elem) => elem.name == info.name);
+            if (obj == null)
+            {
+                throw new Exception(string.Format("{0}:存在しません", info.name));
+            }
+            else
+            {
+                obj.copy(info);
+            }
+        }
+
+        public void add_info(ExecCommandInfo info)
+        {
+            ExecCommandInfo obj = this._infos.FirstOrDefault((ExecCommandInfo elem) => elem.name == info.name);
+            if (obj == null)
+            {
+                _infos.Add(info);
+            }
+            else
+            {
+                throw new Exception(string.Format("{0}:存在します", info.name));
+            }
+
+        }
+
+        public void delete_info(string name)
+        {
+            ExecCommandInfo obj = _infos.FirstOrDefault((ExecCommandInfo elem) => elem.name == name);
+            if (obj != null)
+            {
+                _infos.Remove(obj);
+            }
+        }
+
+        public void load_infos(string xml_file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ExecCommandInfo>));
+            StreamReader sr = new StreamReader("infos.xml", new UTF8Encoding(false));
+            this._infos = (List<ExecCommandInfo>)serializer.Deserialize(sr);
+            sr.Close();
+        }
+
+        public void save_infos(string xml_file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<ExecCommandInfo>));
+            StreamWriter sw = new StreamWriter(xml_file, false, new UTF8Encoding(false));
+            serializer.Serialize(sw, this._infos);
+            sw.Close();
+        }
+
+
+
+    }
+
+    public class ExecCommandInfo
+    {
+        private string _name;
+        private string _commandLine;
+        private string _exec_dir;
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        public string commandLine{
+            get { return _commandLine; }
+            set {_commandLine=value;}
+        }
+        public string exec_dir
+        {
+            get { return _exec_dir; }
+            set { _exec_dir = value; }
+        }
+
+        public ExecCommandInfo()
+        {
+
+        }
+
+        public ExecCommandInfo(string name,string commandLine,string exec_dir)
+        {
+            _name = name;
+            _commandLine = commandLine;
+            _exec_dir = exec_dir;
+        }
+
+        public void copy(ExecCommandInfo info)
+        {
+            _name = info.name;
+            _commandLine = info.commandLine;
+            _exec_dir = info.exec_dir;
+        }
+    }
+}
